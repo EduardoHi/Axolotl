4d404b3f0f3cc4b8e9ce7f53dcdeb6186b0ee383
kawer <kawer7@gmail.com>
2019-03-10 19:27:10 -0600

    Adds tests for literals and tokens

--- test/LexerSpec.hs
+++ test/LexerSpec.hs
@@ -11,11 +11,14 @@ import Axo.Parser
 lexerSpec :: Spec
 lexerSpec = do
   intSpec
+  floatSpec
+  charSpec
+  stringSpec
   varIdSpec
-
+  typeIdSpec
 
 intSpec = do
-  describe "parseInt" $ do
+  describe "Integer parser" $ do
 
     -- Succeses
     
@@ -27,9 +30,72 @@ intSpec = do
     it "fails on a letter" $
       parse intLit "" `shouldFailOn` "abc"
 
+floatSpec = do
+  describe "Float parser" $ do
+
+    -- Succeses
+
+    it "parses a float literal in decimal representation" $
+      parse floatLit "" "123.456" `shouldParse` (FloatLit "123.456")
+    
+    it "parses the float literal 2.0" $
+      parse floatLit "" "2.0"  `shouldParse` (FloatLit "2.0")
+
+    -- Failures
+
+    it "fails on integer" $
+      parse floatLit "" `shouldFailOn` "2"
+
+charSpec = do
+  describe "Character parser" $ do
+
+    -- Succeses
+
+    it "parses an alphabetic character" $
+      parse charLit "" "\'a\'" `shouldParse` (CharLit 'a')
+
+    it "parses a numeric character" $
+      parse charLit "" "\'4\'" `shouldParse` (CharLit '4')
+
+    it "parses a non alphanumeric character" $
+      parse charLit "" "\'%\'" `shouldParse` (CharLit '%')
+
+    -- Failures
+
+    it "fails on more than one character" $
+      parse charLit "" `shouldFailOn` "a!"
+
+    it "fails if there is no closing single quote" $
+      parse charLit ""  `shouldFailOn` "\' "
+      
+stringSpec = do
+  describe "String parser" $ do
+
+    -- Succeses
+
+    it "parses a string with all uppercase letters" $
+      parse stringLit "" "\"AXOLOTL\"" `shouldParse` (StringLit "AXOLOTL")
+
+    it "parses a string with all lowercase letters" $
+      parse stringLit "" "\"this is a lowercase string\"" `shouldParse` (StringLit "this is a lowercase string")
+
+    it "parses a string with only non alphanumeric characters" $
+      parse stringLit "" "\"!@#$%^&\"" `shouldParse` (StringLit "!@#$%^&")
+    
+    it "parses a string with the parenthises characters" $
+      parse stringLit "" "\"() {}\"" `shouldParse` (StringLit "() {}")
+
+    it "parses a string with escaped characters" $
+      parse stringLit "" "\"There is an escaped char \n\""  `shouldParse` (StringLit "There is an escaped char \n")
+
+    -- Failures
+
+    it "fails if there are no closing quotes" $
+      parse stringLit "" `shouldFailOn` "\" " 
+
 
 varIdSpec = do
-  describe "varId" $ do
+  describe "VarId parser" $ do
 
     -- Succeses
     
@@ -53,4 +119,41 @@ varIdSpec = do
     it "fails on a PascalCase name" $
       parse varId "" `shouldFailOn` "Abc"
 
+    it "fails if name starts with a number" $
+      parse varId "" `shouldFailOn` "4abc"
+    
+    it "fails if there are spaces in the string" $
+      parse varId "" `shouldFailOn` "Space here"
+    
+    it "fails if there are combined alphanumeric and non-alphanumeric characters" $
+      parse varId "" `shouldFailOn` "a#c!"
 
+
+typeIdSpec = do
+  describe "TypeId parser" $ do
+
+  -- Succeses
+
+    it "parses an all uppercase name" $
+      parse typeId "" "ABCD" `shouldParse` (TypeId "ABCD")
+
+    it "parses a PascalCase name" $
+      parse typeId "" "PascalCase" `shouldParse` (TypeId "PascalCase")
+
+    it "cuts the parsing when it meets a reserved symbol" $
+      parse typeId "" "Abc)d" `shouldParse` (TypeId "Abc")
+
+    -- Failures
+
+    it "fails if name starts with a number" $
+      parse typeId "" `shouldFailOn` "4abc"
+    
+    it "fails if name begins with a non-alphanumeric character" $
+      parse typeId "" `shouldFailOn` "#Abc"
+
+    it "fails if name contains a non-alphanumeric character" $
+      parse typeId "" `shouldFailOn` "Ab#c"
+
+    it "fails if there are spaces in the string" $
+      parse typeId "" `shouldFailOn` "Space here"
+  
